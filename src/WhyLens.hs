7882a6d1b178fccc025c39f89faedbcf1908754c
Pawel Szulc <paul.szulc@gmail.com>
2020-07-27 21:58:09 +0200

    Conference - example structure

---
+++ src/WhyLens.hs
@@ -0,0 +1,31 @@
+module WhyLens where
+
+data Conference = Conference
+  { organizer :: Organizer
+  , speakers  :: [Speaker]
+  } deriving Show
+
+data Organizer = Organizer
+  { name    :: Name
+  , contact :: Contact
+  } deriving Show
+
+data Name = Name
+  { firstName :: String
+  , lastName  :: String
+  } deriving Show
+
+data Contact = Contact
+  { address :: Address
+  , email   :: String
+  } deriving Show
+
+data Speaker = Speaker
+  { slidesReady :: Bool
+  } deriving Show
+
+data Address = Address
+  { street  :: String
+  , city    :: String
+  , country :: String
+  } deriving Show
